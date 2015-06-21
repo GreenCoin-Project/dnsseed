aa9daca202be57883b0c0241a48aa6d16ed086e8
Nilam Doctor <nilamdoc@gmail.com>
2015-06-21 12:09:06 +0530

    hitarth

--- main.cpp
+++ main.cpp
@@ -339,8 +339,8 @@ extern "C" void* ThreadStats(void*) {
   } while(1);
 }
 
-static const string mainnet_seeds[] = {"dnsseed.bluematt.me", "bitseed.xf2.org", "dnsseed.bitcoin.dashjr.org", "seed.bitcoin.sipa.be", ""};
-static const string testnet_seeds[] = {"testnet-seed.bitcoin.petertodd.org", "static-testnet-seed.bitcoin.petertodd.org", ""};
+static const string mainnet_seeds[] = {"seed.hitarth.org", ""};
+static const string testnet_seeds[] = {"testnet-seed.hitarth.org""};
 static const string *seeds = mainnet_seeds;
 
 extern "C" void* ThreadSeeder(void*) {
